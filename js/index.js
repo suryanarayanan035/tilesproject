4eaa2f07d63eddcfd92105fe6a2df7b8583f907a
Darkie <darkie@Kali.localhost>
2020-06-21 20:50:17 +0530

    sample desgns completed

--- js/index.js
+++ js/index.js
@@ -2,8 +2,9 @@
 function changePhoto(el)
 {
     var new_src = $(el).attr("src");
-    regex=/.jpeg | .jpg/
-    temp = new_src.split(".jpeg")
-    new_src=temp[0]+temp[1]+"home.jpeg";
-    $("#house-image").attr("src",new_src);
+    temp = new_src.split(".jpg")
+    lr_src=temp[0]+temp[1]+"lr.jpg";
+    br_src=temp[0]+temp[1]+"br.jpg";
+    $("#lr-image").attr("src",lr_src);
+    $("#br-image").attr("src",br_src);
 }
